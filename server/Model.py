11a3f05433499fedbf1b13dc3d57984de7a0a035
iamwatchdogs <shamith3011022@gmail.com>
2024-04-11 16:38:58 +0530

    Updated Model.py

--- server/Model.py
+++ server/Model.py
@@ -4,27 +4,23 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from skimage.transform import resize
 
 
-def predict(image_path, exp_cdr, type, set):
+def preprocess_image(image_path, target_size):
+    image = load_img(image_path)
+    image = img_to_array(image)
+    image = image / 255.0
+    image = resize(image, target_size)
+    return image
+
+def predict(image, exp_cdr, type, set):
 
     EXPORTED_MODEL = load_model("model/exports/glaucoma_detection_model_vgg.h5")
     IMAGE_TARGET_SIZE = (150, 150)
 
-    def preprocess_image(image_path, target_size):
-        image = load_img(image_path)
-        image = img_to_array(image)
-        image = image / 255.0
-        image = resize(image, target_size)
-        return image
+    # Checking if it's a path or preprocessed image
+    if '/' in image or '\\' in image:
+        image = preprocess_image(image, IMAGE_TARGET_SIZE)
+    attributes = [exp_cdr, type, set]
 
-
-    def preprocess_attributes(exp_cdr, type, set):
-        processed_attributes = [exp_cdr, type, set]
-        return processed_attributes
-
-    preprocessed_image = preprocess_image(image_path, IMAGE_TARGET_SIZE)
-    preprocessed_attributes = preprocess_attributes(exp_cdr, type, set)
-
-    prediction = EXPORTED_MODEL.predict(
-        [np.array([preprocessed_image]), np.array([preprocessed_attributes])])
+    prediction = EXPORTED_MODEL.predict([np.array([image]), np.array([attributes])])
 
     return prediction[0][0]
