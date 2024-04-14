3d9d9887e20eadfd120bc8e06767e3e8c537795c
iamwatchdogs <shamith3011022@gmail.com>
2024-04-15 00:27:32 +0530

    Completed the Home Component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,3 +1,31 @@
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./style.css";
+
 export default function Home() {
-  return "Home";
+  return (
+    <>
+      <Container id='home'>
+        <Row>
+          <Col id='context'>
+            <h1>Welcome to GlaucoDetect</h1>
+            <p>
+              Glaucoma is a slient killer of the world you see. It{"'"}s hard to
+              spot and even harder to treat once it takes hold. You can treat it
+              only at it{"'"}s early stages. How can you detect it when really
+              hard to spot ?
+            </p>
+            <p>
+              Preseting you <span>GlaucoDetect</span>, an AI-powered web
+              application that helps you get your result that are accurate and
+              fast even without consulting a doctor. Check your eye for free
+              today, {"'"}cause A stitch in time saves nine.
+            </p>
+            <Button><Link to="/predict">Check you eye</Link></Button>
+          </Col>
+          <Col id='img'></Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
