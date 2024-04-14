987f64d0afeef5bf77923b02e9d8a23b569b932b
iamwatchdogs <shamith3011022@gmail.com>
2024-04-15 00:25:39 +0530

    Updated semantics of App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,12 @@ import { Outlet } from "react-router-dom";
 export default function App() {
   return (
     <>
-      <Header />
-      <Outlet />
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
