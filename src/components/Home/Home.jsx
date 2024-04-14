b7dbfbee098432dff288502b9b1daaf0e0bfe8ca
iamwatchdogs <shamith3011022@gmail.com>
2024-04-15 00:33:20 +0530

    Removed fragment

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,28 +4,28 @@ import "./style.css";
 
 export default function Home() {
   return (
-    <>
-      <Container id='home'>
-        <Row>
-          <Col id='context'>
-            <h1>Welcome to GlaucoDetect</h1>
-            <p>
-              Glaucoma is a slient killer of the world you see. It{"'"}s hard to
-              spot and even harder to treat once it takes hold. You can treat it
-              only at it{"'"}s early stages. How can you detect it when really
-              hard to spot ?
-            </p>
-            <p>
-              Preseting you <span>GlaucoDetect</span>, an AI-powered web
-              application that helps you get your result that are accurate and
-              fast even without consulting a doctor. Check your eye for free
-              today, {"'"}cause A stitch in time saves nine.
-            </p>
-            <Button><Link to="/predict">Check you eye</Link></Button>
-          </Col>
-          <Col id='img'></Col>
-        </Row>
-      </Container>
-    </>
+    <Container id='home'>
+      <Row>
+        <Col id='context'>
+          <h1>Welcome to GlaucoDetect</h1>
+          <p>
+            Glaucoma is a slient killer of the world you see. It{"'"}s hard to
+            spot and even harder to treat once it takes hold. You can treat it
+            only at it{"'"}s early stages. How can you detect it when really
+            hard to spot ?
+          </p>
+          <p>
+            Preseting you <span>GlaucoDetect</span>, an AI-powered web
+            application that helps you get your result that are accurate and
+            fast even without consulting a doctor. Check your eye for free
+            today, {"'"}cause A stitch in time saves nine.
+          </p>
+          <Button>
+            <Link to='/predict'>Check you eye</Link>
+          </Button>
+        </Col>
+        <Col id='img'></Col>
+      </Row>
+    </Container>
   );
 }
