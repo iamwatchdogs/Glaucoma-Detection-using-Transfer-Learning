6edaace1f2b167f7b404b620ee916ec5c6e2a0e3
iamwatchdogs <shamith3011022@gmail.com>
2024-04-14 13:02:56 +0530

    Finished the server logic

---
+++ server/server.py
@@ -0,0 +1,48 @@
+import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+from Model import predict
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/classify')
+def classify_page():
+    return '<h1>Only POST requests are allowed on this endpoint !!!...</h1>'
+
+@app.route('/classify', methods=['POST'])
+def classify():
+
+    # Log the request
+    app.logger.info("Received request: %s", request)
+    app.logger.info("Request Header: %s", request.headers)
+
+    # extracting data from the request
+    exp_cdr = float(request.form.get('expcdr'))
+    type = int(request.form.get('type'))
+    set = int(request.form.get('set'))
+    app.logger.info("Extracted non-file attributes: \n%s", str({"expcdr": exp_cdr, "type": type, "set": set}))
+
+    # save image to upload folder
+    filepath = request.files['img']
+    filepath.save('uploads/image.jpg')
+    app.logger.info("Saved Image at: %s", 'uploads/image.jpg')
+
+    # Using the Model
+    result = str(predict('uploads/image.jpg', exp_cdr, type, set))
+    app.logger.info("Model successfully predicted: %s", result)
+
+    # Clean up
+    os.remove('uploads/image.jpg')
+    app.logger.info("Deleted image: %s", 'uploads/image.jpg')
+
+
+    app.logger.info("Sending Response back to the client...")
+    return jsonify({'prediction': result})
+
+
+if __name__ == '__main__':
+    os.makedirs('uploads', exist_ok=True)
+    app.run(debug=True, host='127.0.0.1', port=5000)
+    os.rmdir('uploads')
