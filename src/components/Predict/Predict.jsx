da2c91e6a66f0dbfd88a3c253c6887feee42f40d
iamwatchdogs <shamith3011022@gmail.com>
2024-04-17 00:43:06 +0530

    Added the FormComponent into Predict component

--- src/components/Predict/Predict.jsx
+++ src/components/Predict/Predict.jsx
@@ -1,3 +1,15 @@
-export default function Predict(){
-	return "Predict";
+import { Container, Row, Col } from "react-bootstrap";
+import FormComponent from "./FormComponent";
+
+export default function Predict() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Predict</h1>
+          <FormComponent />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
