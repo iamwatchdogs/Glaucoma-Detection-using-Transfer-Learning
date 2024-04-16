2dc84d408497c7dcfbf0543fc413b8287602935c
iamwatchdogs <shamith3011022@gmail.com>
2024-04-17 00:33:49 +0530

    Added body key mapping

--- server/server.py
+++ server/server.py
@@ -7,6 +7,14 @@ from Model import predict
 app = Flask(__name__)
 CORS(app)
 
+# Body key mapping
+BODY_KEYS = {
+    'expcdr': 'ExpCDR',
+    'type': 'EyeType',
+    'set': 'EyeSet',
+    'img': 'ImgFile'
+}
+
 @app.route('/classify')
 def classify_page():
     return '<h1>Only POST requests are allowed on this endpoint !!!...</h1>'
@@ -17,15 +25,17 @@ def classify():
     # Log the request
     app.logger.info("Received request: %s", request)
     app.logger.info("Request Header: %s", request.headers)
+    app.logger.info("Request Body: %s", request.form.keys())
 
     # extracting data from the request
-    exp_cdr = float(request.form.get('expcdr'))
-    type = int(request.form.get('type'))
-    set = int(request.form.get('set'))
+    exp_cdr = float(request.form.get(BODY_KEYS['expcdr']))
+    type = int(request.form.get(BODY_KEYS['type']))
+    set = int(request.form.get(BODY_KEYS['set']))
     app.logger.info("Extracted non-file attributes: \n%s", str({"expcdr": exp_cdr, "type": type, "set": set}))
 
     # save image to upload folder
-    filepath = request.files['img']
+    os.makedirs('uploads', exist_ok=True)
+    filepath = request.files[BODY_KEYS['img']]
     filepath.save('uploads/image.jpg')
     app.logger.info("Saved Image at: %s", 'uploads/image.jpg')
 
@@ -36,10 +46,8 @@ def classify():
     # Clean up
     os.remove('uploads/image.jpg')
     app.logger.info("Deleted image: %s", 'uploads/image.jpg')
-
-
     app.logger.info("Sending Response back to the client...")
-    return jsonify({'prediction': result})
+    return jsonify({"prediction": result})
 
 
 if __name__ == '__main__':
