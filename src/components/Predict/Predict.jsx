eb4255ebac8a827bd29e1c608d40a1d4c36f3197
iamwatchdogs <shamith3011022@gmail.com>
2024-04-18 02:55:04 +0530

    Added a placeholder lorem for description

--- src/components/Predict/Predict.jsx
+++ src/components/Predict/Predict.jsx
@@ -2,14 +2,17 @@ import { Container, Row, Col } from "react-bootstrap";
 import { useState } from "react";
 
 import FormComponent from "./FormComponent";
+import './style.css';
 
 export default function Predict() {
   const [result, setResult] = useState();
   return (
-    <Container>
+    <Container id="main">
       <Row>
         <Col>
           <h1>Predict</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et rerum explicabo recusandae laboriosam placeat sint eos, assumenda ipsa facere repellendus repellat nemo, consequuntur dolores ipsam enim illum! Molestias, quidem voluptatem.
+          Nam suscipit numquam obcaecati, ex omnis inventore culpa sequi tempore mollitia quibusdam sunt molestias quisquam repellendus, tempora maxime cum? Qui culpa tempore corporis maiores perferendis? Quia repudiandae itaque repellendus optio.</p>
           <FormComponent setResult={setResult} />
         </Col>
         <Col>{result !== undefined && <h2>Result: {result}</h2>}</Col>
