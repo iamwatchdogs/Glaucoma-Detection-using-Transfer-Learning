eda3f8494dbb5c8836e83aa1abdbd6fe86b5a0d1
iamwatchdogs <shamith3011022@gmail.com>
2024-04-18 02:48:39 +0530

    Added a dark linear gradient for remaining pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Header from "./components/Header/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 export default function App() {
+  const location = useLocation();
+  const darkStyling = `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.10)),
+    linear-gradient(
+      140deg,
+      #eadedb 0%,
+      #fb6d7a 25%,
+      #bc70a4 50%,
+      #eff5d0 75%,
+      #0f7394
+    )`;
   return (
     <>
       <header>
         <Header />
       </header>
-      <main>
+      <main style={location.pathname === '/' ? {} : {backgroundImage: darkStyling}}>
         <Outlet />
       </main>
     </>
