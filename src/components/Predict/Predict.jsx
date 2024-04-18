da1d963ccf749feb6b330dea08d47b1e5feef2b6
iamwatchdogs <shamith3011022@gmail.com>
2024-04-18 23:58:05 +0530

    Updated predict page content

--- src/components/Predict/Predict.jsx
+++ src/components/Predict/Predict.jsx
@@ -2,17 +2,24 @@ import { Container, Row, Col } from "react-bootstrap";
 import { useState } from "react";
 
 import FormComponent from "./FormComponent";
-import './style.css';
+import "./style.css";
 
+// https://pubmed.ncbi.nlm.nih.gov/21095735/
 export default function Predict() {
   const [result, setResult] = useState();
   return (
-    <Container id="main">
+    <Container id='main'>
       <Row>
         <Col>
-          <h1>Predict</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et rerum explicabo recusandae laboriosam placeat sint eos, assumenda ipsa facere repellendus repellat nemo, consequuntur dolores ipsam enim illum! Molestias, quidem voluptatem.
-          Nam suscipit numquam obcaecati, ex omnis inventore culpa sequi tempore mollitia quibusdam sunt molestias quisquam repellendus, tempora maxime cum? Qui culpa tempore corporis maiores perferendis? Quia repudiandae itaque repellendus optio.</p>
+          <h1>Check your Eye</h1>
+          <p>
+            To analyze the condition of the eye, the ML model mainly requires
+            your retainal scan. Other than your image of scan, the model also
+            required few other diagnostic result such as ExpCDR (Cup-to-disc
+            ratio), Eye Type (left eye (OS) or right eye (OD)) and Eye Set
+            (Usual given along with retainal scan, they{"'"}re just group
+            assiged based on Intraocular Pressure (IOP) & Corneal Thickness).
+          </p>
           <FormComponent setResult={setResult} />
         </Col>
         <Col>{result !== undefined && <h2>Result: {result}</h2>}</Col>
